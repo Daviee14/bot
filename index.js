9d4b452655f43de4083eacd2956a49ea65ae121d
David Ajanaku <davidajanaku46163@gmail.com>
2025-05-11 01:01:29 +0100

    `Added Discord selfbot functionality, including keep-alive server, guild member join event handler, and direct message handler.`

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const Discord = require("discord.js-selfbot-v13");
 const keepAlive = require("./server.js"); // Import the keep-alive server
+
 const client = new Discord.Client({
   checkUpdate: false,
 });
@@ -10,6 +11,7 @@ const token = process.env.TOKEN;
 if (!token) {
   throw new Error("Discord TOKEN not found in environment variables");
 }
+
 // Start the keep-alive server
 keepAlive();
 
@@ -17,11 +19,25 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
+// Handle when users join a server
 client.on("guildMemberAdd", (member) => {
   console.log(`${member.user.tag} joined ${member.guild.name}`);
   const channel = client.channels.cache.get("1370018863491842128");
-  if (channel)
-    channel.send(`User ${member.user.tag} joined ${member.guild.name}`);
+  if (channel) {
+    // Format with mention using <@userId> syntax
+    channel.send(`User <@${member.user.id}> (${member.user.tag}) joined ${member.guild.name}`);
+  }
+});
+
+// Handle direct messages - show username mentions
+client.on("messageCreate", (message) => {
+  // Only process DMs (not from self)
+  if (message.channel.type === "DM" && message.author.id !== client.user.id) {
+    console.log(`Received DM from ${message.author.tag}`);
+    
+    // Reference the user with proper mention format
+    message.channel.send(`I received your message, <@${message.author.id}>! I can see your profile now.`);
+  }
 });
 
 // Auto-reconnect logic
